142ef08b8e2a76ffd2b904700226d2c58e97c3b7
Tobias Waslowski <contact@twaslowski.com>
2025-07-07 21:06:42 +0200

    feat: introduce crunchy postgres-operator

---
+++ terraform/core/postgres-operator.tf
@@ -0,0 +1,8 @@
+resource "helm_release" "postgres_operator" {
+  name  = "postgres-operator"
+  chart = "../charts/pgo"
+  namespace = "postgres-operator"
+  create_namespace = true
+
+  values = [file("${path.module}/config/values/postgres-operator.values.yaml")]
+}
