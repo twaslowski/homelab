b49b74a7ce1177d904132b3b247987964a50f71c
Tobias Waslowski <contact@twaslowski.com>
2025-07-10 16:42:14 +0200

    feat: migrate prometheus and grafana

--- terraform/core/prometheus.tf
+++
@@ -1,9 +0,0 @@
-resource "helm_release" "prometheus" {
-  name       = "prometheus"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "prometheus"
-
-  version          = "27.22.0"
-  namespace        = "prometheus"
-  create_namespace = true
-}
