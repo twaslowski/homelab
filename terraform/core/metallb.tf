f1301d995b72d3070df265d5f13b6234a82273c1
Tobias Waslowski <contact@twaslowski.com>
2025-07-10 17:23:52 +0200

    chore: migrate metallb

--- terraform/core/metallb.tf
+++
@@ -1,9 +0,0 @@
-resource "helm_release" "metal_lb" {
-  name       = "metallb"
-
-  repository = "https://metallb.github.io/metallb"
-  chart      = "metallb"
-  namespace  = "kube-system"
-
-  version    = "0.15.2"
-}
--- terraform/core/postgres-operator.tf
+++
@@ -1,8 +0,0 @@
-resource "helm_release" "postgres_operator" {
-  name  = "postgres-operator"
-  chart = "../../charts/pgo"
-  namespace = "postgres-operator"
-  create_namespace = true
-
-  values = [file("${path.module}/config/values/postgres-operator.values.yaml")]
-}
